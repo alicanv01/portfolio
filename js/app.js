4ac4ddf85a52ffe2db35939524888e16284f9a9d
alican1809 <94050902+alican1809@users.noreply.github.com>
2023-11-16 13:09:34 +0100

    add animation css

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// animation font ***/AlicnYEsilkaya a faire
+//const video = document.querySelector(".video-container video");
+//const hoverText = document.querySelector(".video-container .hover-text");
+//
+//video.addEventListener("mouseenter", () => {
+//  video.play();
+//  hoverText.classList.remove("active");
+//});
+//
+//video.addEventListener("mouseleave", () => {
+//  video.pause();
+//  hoverText.classList.add("active");
+//});
