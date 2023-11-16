d3e50b403bec554a1779f08f23a0d227f884608a
alican1809 <94050902+alican1809@users.noreply.github.com>
2023-11-17 00:19:09 +0100

    v1

--- js/app.js
+++ js/app.js
@@ -11,3 +11,18 @@
 //  video.pause();
 //  hoverText.classList.add("active");
 //});
+
+
+
+//      // Récupérer l'élément source
+//      var vBg = document.getElementById("vBg");
+//
+//      // Récupérer la hauteur de l'élément source
+//      var height = vBg.offsetHeight;
+//
+//      // Récupérer l'élément cible
+//      var filtre = document.getElementById("filtre");
+//
+//      // Affecter la hauteur à l'élément cible
+//      filtre.style.height = height + 'px';
+
