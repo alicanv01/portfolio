bc62188cb33fff09d57bf1c50bc3668a9ca887fa
alican1809 <94050902+alican1809@users.noreply.github.com>
2023-11-17 22:40:49 +0100

    add json system

--- js/app.js
+++ js/app.js
@@ -1,28 +1,64 @@
-// animation font ***/AlicnYEsilkaya a faire
-//const video = document.querySelector(".video-container video");
-//const hoverText = document.querySelector(".video-container .hover-text");
-//
-//video.addEventListener("mouseenter", () => {
-//  video.play();
-//  hoverText.classList.remove("active");
-//});
-//
-//video.addEventListener("mouseleave", () => {
-//  video.pause();
-//  hoverText.classList.add("active");
-//});
+var projectCard = document.querySelectorAll(`.projetCard`);
+var slideProjetCardNbr = projectCard.length;
+console.log(slideProjetCardNbr);
 
-
-
-//      // Récupérer l'élément source
-//      var vBg = document.getElementById("vBg");
+//for (let index = 0; index < slideProjetCardNbr; index++) {
+//    for (const key in data.gif) {
+//        if (data.gif.hasOwnProperty.call(key)) {
+//            const item = data.gif[key];
 //
-//      // Récupérer la hauteur de l'élément source
-//      var height = vBg.offsetHeight;
-//
-//      // Récupérer l'élément cible
-//      var filtre = document.getElementById("filtre");
-//
-//      // Affecter la hauteur à l'élément cible
-//      filtre.style.height = height + 'px';
+//        }
+//    }
+//}       
+           
 
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(
+    "https://raw.githubusercontent.com/alicanv01/projetmedia/main/media.json"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Assurez-vous que la clé "gif" existe dans le fichier JSON
+      if (data.gif) {
+        // Boucle à travers chaque élément dans l'objet "gif"
+
+        for (const key in data.gif) {
+          if (data.gif.hasOwnProperty(key)) {
+            const item = data.gif[key];
+
+         var slideProjetCardNbr = projectCard.length;
+            console.log(slideProjetCardNbr);
+            
+            projectCard.forEach(function(element, index){
+            
+         
+            var span=document.createElement('span')
+            span.className = "slide-content v";
+            element.appendChild(span)
+            var img =document.createElement('img');
+            img.className=("gif");
+            img.setAttribute("src",`${item.link}`);
+            span.appendChild(img)
+            }
+              
+            );
+
+            // Affichez les informations dans la console (adaptez cela à votre structure HTML)
+            console.log(
+              `Type: ${item.type}, Titre: ${item.title}, Lien: ${item.link}`
+            );
+          }
+        }
+      } else {
+        console.error("La clé 'gif' est manquante dans le fichier JSON");
+      }
+    })
+    .catch((error) =>
+      console.error("Erreur de chargement du fichier JSON", error)
+    );
+});
